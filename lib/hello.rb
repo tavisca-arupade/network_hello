048b1974d518ba1193677424dea77fdb9d1962fb
Aditi <arupade@tavisca.in>
2019-06-18 23:25:46 +0530

    Hello uses Greeter

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,4 +1,8 @@
+require 'greeter'
+
 # Default is "World"
 #Author : Aditi Rupade (arupade@tavisca.in)
 name = ARGV.first || "World"
-puts "Hello, #{name}!"
+
+greeter = Greeter.new(name)
+puts greeter.greet
